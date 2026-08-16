c56bc1bbd6994cdd2dcf788196951a136c5f8d8c
korithespirit <korithespirit@gmail.com>
2026-08-16 10:47:13 +0700

    feat(Serial,main): Init Serial UART in main.c

--- main.c
+++ main.c
@@ -2,19 +2,22 @@
 
 void setup()
 {
-  // Khởi tạo LCD
-  LCD_Init();
+
+    Serial.begin(9600);
+    // Khởi tạo LCD
+    LCD_Init();
 
 
-  // Bật Display
-  // 0x0C = Display ON
-  // Cursor OFF
-  // Blink OFF
-  SendByteToLCD(0x0C, false);
+    // Bật Display
+    // 0x0C = Display ON
+    // Cursor OFF
+    // Blink OFF
+    SendByteToLCD(0x0C, false);
 
 
-  // Test gửi ký tự A
-  LCD_WriteChar('A');
+    // Test gửi ký tự A
+    LCD_WriteChar('A');
+    Serial.println("HelloWorld")
 }
 
 void loop()
